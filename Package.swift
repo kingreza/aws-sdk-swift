7c796f5b1f6c567088202cb54431b0559105a13a
Adam Fowler <adamfowler71@gmail.com>
2020-05-01 09:47:43 +0100

    Use date-encoding branch of aws-sdk-swift-core

--- Package.swift
+++ Package.swift
@@ -228,7 +228,7 @@ let package = Package(
         .library(name: "AWSXRay", targets: ["AWSXRay"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .branch("user-properties"))
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .branch("date-encoding"))
     ],
     targets: [
         .target(name: "AWSACM", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/AWSSDKSwift/Services/ACM"),
